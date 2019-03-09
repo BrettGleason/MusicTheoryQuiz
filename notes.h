421b6f64db82b63fc387264d8987c2e499f627a5
Brett Gleason <bmgdx8@mail.missouri.edu>
2019-03-09 09:38:49 -0600

    Refactor notes.h
    Add overload to noteName() function so that it can use the integer value
    of the enum item in addition to the enum reference. This allows the note
    name to be retrieved after math is done on the original value.

--- notes.h
+++ notes.h
@@ -38,4 +38,35 @@ std::string noteName(Note noteEnumValue)
     return note;
 }
 
+std::string noteName(int noteValue)
+{
+    std::string note;
+
+    switch (noteValue)
+    {
+        case static_cast<int>(Note::A):
+            note = "A";
+            break;
+        case static_cast<int>(Note::B):
+            note = "B";
+            break;
+        case static_cast<int>(Note::C):
+            note = "C";
+            break;
+        case static_cast<int>(Note::D):
+            note = "D";
+            break;
+        case static_cast<int>(Note::E):
+            note = "E";
+            break;
+        case static_cast<int>(Note::F):
+            note = "F";
+            break;
+        case static_cast<int>(Note::G):
+            note = "G";
+            break;
+    }
+
+    return note;
+}
 #endif
