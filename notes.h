20f989a53dde3c9b0427bd5ada893bc9e13d6cf1
Brett Gleason <bmgdx8@mail.missouri.edu>
2019-03-09 08:52:49 -0600

    Refactor notes.h
    Make Note enum into an enum class so that its values are scoped and
    strongly typed.
    Add noteName() function to convert an enum value to its name. The better
    way to handle this would be to use a template or an operator overload,
    but because I don't expect to have to add more values to the set of
    note names I'm not concerned that the function will become
    unsynchronized with the enum.

--- notes.h
+++ notes.h
@@ -1,6 +1,41 @@
 #ifndef notes_h
 #define notes_h
 
-enum notes {A, B, C, D, E, F, G};
+#include <iostream>
+#include <string>
+
+enum class Note {A, B, C, D, E, F, G};
+
+std::string noteName(Note noteEnumValue)
+{
+    std::string note;
+
+    switch (noteEnumValue)
+    {
+        case Note::A:
+            note = "A";
+            break;
+        case Note::B:
+            note = "B";
+            break;
+        case Note::C:
+            note = "C";
+            break;
+        case Note::D:
+            note = "D";
+            break;
+        case Note::E:
+            note = "E";
+            break;
+        case Note::F:
+            note = "F";
+            break;
+        case Note::G:
+            note = "G";
+            break;
+    }
+
+    return note;
+}
 
 #endif
